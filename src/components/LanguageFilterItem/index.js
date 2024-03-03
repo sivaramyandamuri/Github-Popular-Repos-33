268fa8438a08c0a1d4469a9686f550ea5811c276
Sivaram <ysivaram42@gmail.com>
2024-03-03 05:09:15 +0000

    Github Popular Repos

---
+++ src/components/LanguageFilterItem/index.js
@@ -0,0 +1,27 @@
+// Write your code here
+import './index.css'
+
+const LanguageFilterItem = props => {
+  const {eachTabDetails, updateActiveTabId, activeTabId} = props
+  const {id, language} = eachTabDetails
+
+  const onClickTabItem = () => {
+    updateActiveTabId(id)
+  }
+
+  const tabStyleClassName = activeTabId === id ? 'active-tab' : ''
+
+  return (
+    <li className="list-item">
+      <button
+        type="button"
+        className={`tab-button ${tabStyleClassName}`}
+        onClick={onClickTabItem}
+      >
+        {language}
+      </button>
+    </li>
+  )
+}
+
+export default LanguageFilterItem
